3ddb59cbe63d72892439dac0cf8368fdb2bf4557
Gabriel Medeiros <gabrielmedeirosn05@gmail.com>
2021-12-01 20:48:35 -0300

    Esqueleto inicial do projeto PagSystem

---
+++ resources/frontend/angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopMenuComponent } from './menus/top_menu/top-menu/top-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
