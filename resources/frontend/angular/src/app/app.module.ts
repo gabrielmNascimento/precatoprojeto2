3da4d348f3e673d95b0acbbd6a34a9bce2ef7453
Gabriel Medeiros <gabrielmedeirosn05@gmail.com>
2021-12-13 20:33:23 -0300

    Limpando para começar os menus

--- resources/frontend/angular/src/app/app.module.ts
+++ resources/frontend/angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './menus/top_menu/top-menu/top-menu.component';
+import { SidebarComponent } from './menus/sidebar/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
