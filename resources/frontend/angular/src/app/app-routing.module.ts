b9a7ab9b6a511c936ebb98ff75b9a4fc176f041c
Gabriel Medeiros <gabrielmedeirosn05@gmail.com>
2022-01-01 11:18:54 -0300

    Criando views, components e rotas

--- resources/frontend/angular/src/app/app-routing.module.ts
+++ resources/frontend/angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CobrarComponent } from './views/cobrar/cobrar/cobrar.component';
+import { DashboardsComponent } from './views/dashboards/dashboards/dashboards.component';
+import { SolicitarComponent } from './views/solicitar/solicitar/solicitar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboards', component: DashboardsComponent},
+  {path: 'solicitar', component: SolicitarComponent},
+  {path: 'cobrar', component: CobrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- resources/frontend/angular/src/app/app.module.ts
+++ resources/frontend/angular/src/app/app.module.ts
@@ -7,12 +7,20 @@ import { TopMenuComponent } from './menus/top_menu/top-menu/top-menu.component';
 import { SidebarComponent } from './menus/sidebar/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './icons/angular-material.module';
+import { PageComponent } from './generic/page/page/page.component';
+import { CobrarComponent } from './views/cobrar/cobrar/cobrar.component';
+import { DashboardsComponent } from './views/dashboards/dashboards/dashboards.component';
+import { SolicitarComponent } from './views/solicitar/solicitar/solicitar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
-    SidebarComponent
+    SidebarComponent,
+    PageComponent,
+    CobrarComponent,
+    DashboardsComponent,
+    SolicitarComponent
   ],
   imports: [
     BrowserModule,
